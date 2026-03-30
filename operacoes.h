0c42ff096bb72ce2c643bb69acece34e8123054b
davigdomingues <domingues.gabriel.davi@gmail.com>
2026-03-30 17:26:41 -0300

    Operações futuras a serem implementadas.

--- operacoes.h
+++ operacoes.h
@@ -10,3 +10,5 @@ void create(char *arquivoEntrada, char *arquivoSaida);
 void selectAll(char *arquivoEntrada);
 void selectWhere(char *arquivoEntrada, CampoValor *par, int mPares);
 bool deleteWhere(char *arquivoEntrada, CampoValor *pares, int mPares);
+// void insert(char *arquivoEntrada, char *arquivoSaida, CampoValor *pares, int mPares);
+// void update(char *arquivoEntrada, char *arquivoSaida, CampoValor *paresBusca, int mParesBusca, CampoValor *paresUpdate, int mParesUpdate);
