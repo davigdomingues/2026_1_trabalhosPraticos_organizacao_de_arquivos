29f3dca78abf34103f19a5e3c12db8a3d27ae598
Davi Gabriel Domingues <domingues.gabriel.davi@gmail.com>
2026-06-19 00:17:09 -0300

    Estrutura de header de ordenação (funcionalidade ORDER BY).

--- headers/operacoes/ordenacoes.h
+++ headers/operacoes/ordenacoes.h
@@ -12,4 +12,22 @@
  */
 void handleOrderBy();
 
+/**
+ * @brief comparador para qsort() que ordena por codEstacao, jogando nulos (-1) para o final.
+ * A ordenação é crescente, ou seja, o menor código de estação (exceto nulos) fica no início do arquivo.
+ * @param a ponteiro para o primeiro registro a ser comparado
+ * @param b ponteiro para o segundo registro a ser comparado
+ * @return valor negativo se a < b, zero se a == b, valor positivo se a > b, considerando a ordenação por codEstacao e
+ * tratando nulos como maiores que qualquer
+ */
+int compararCodEstacao(const void *a, const void *b);
+
+/**
+ * @brief comparador para qsort() que ordena por codProxEstacao
+ * @param a ponteiro para o primeiro registro a ser comparado
+ * @param b ponteiro para o segundo registro a ser comparado
+ * @return idem a compararCodEstacao(), mas considerando a ordenação por codProxEstacao
+ */
+int compararCodProxEstacao(const void *a, const void *b);
+
 #endif
